73a2271aecba4414c320b9a56f1c2f1b3d0a9754
srjm <cyberpiluha@gmail.com>
2025-10-10 16:07:39 +0300

    3.3 Фиксация изменений на github [#505270 #553018]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Komarov Ilya");
+        System.out.println("03.04.1998");
+}
+}
