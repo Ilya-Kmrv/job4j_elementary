98b44980e1d507dbd88754def9f24e4bc54fbd36
srjm <cyberpiluha@gmail.com>
2025-10-23 17:20:41 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefenition.java
+++ src/main/java/ru/job4j/array/ArrayDefenition.java
@@ -6,7 +6,9 @@ public class ArrayDefenition {
     static float[] prices = new float[40];
 
     public static void main(String[] args) {
-
+        String[] name =new String[4];
+        name[0] = "Хорошего дня всем проверяющим!";
+        System.out.println(name[0]);
         System.out.println("Размер массива равен:  " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
