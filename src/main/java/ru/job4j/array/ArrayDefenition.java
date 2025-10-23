8edd7beeac88f9e59d2542748590f3aa2cd2285c
srjm <cyberpiluha@gmail.com>
2025-10-23 17:10:10 +0300

    Объявление массива [#505220]

--- src/main/java/ru/job4j/array/ArrayDefenition.java
+++ src/main/java/ru/job4j/array/ArrayDefenition.java
@@ -1,7 +1,14 @@
 package ru.job4j.array;
 
 public class ArrayDefenition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    static short[] ages = new short[10];
+    static String[] surnames = new String[100500];
+    static float[] prices = new float[40];
+
+    public static void main(String[] args) {
+
+        System.out.println("Размер массива равен: " + ages.length);
+        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива равен: " + prices.length);
+    }
 }
