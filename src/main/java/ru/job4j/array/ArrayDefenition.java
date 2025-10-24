9a08079d93a515b042bc7aea8e4e20fa54e1644b
srjm <cyberpiluha@gmail.com>
2025-10-24 09:06:41 +0300

    Объявление массива [#505220 #553606]

--- src/main/java/ru/job4j/array/ArrayDefenition.java
+++ src/main/java/ru/job4j/array/ArrayDefenition.java
@@ -1,11 +1,10 @@
 package ru.job4j.array;
 
 public class ArrayDefenition {
-    static short[] ages = new short[10];
-    static String[] surnames = new String[100500];
-    static float[] prices = new float[40];
-
     public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
         String[] name = new String[4];
         name[0] = "Хорошего дня всем проверяющим!";
         System.out.println(name[0]);
