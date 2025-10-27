a01e932b590e6ae3f155f5d5fdb81eaba8a26e1e
srjm <cyberpiluha@gmail.com>
2025-10-27 17:48:05 +0300

    Перевернуть массив [#505231]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
+        temp = array[1];
+        array[1] = array[array.length - 2];
+        array[array.length - 2] = temp;
+        return array;
+    }
+}
