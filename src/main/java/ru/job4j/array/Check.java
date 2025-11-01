25f0c75c610fa86596c7fc838e174ce6862939d2
srjm <cyberpiluha@gmail.com>
2025-11-01 10:09:53 +0300

    Массив заполнен true или false [#505232 #553820]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -6,7 +6,7 @@ public class Check {
             return true;
         }
         for (int i = 1; i < data.length; i++) {
-            if (data[i] != data[i-1]) {
+            if (data[i] != data[i - 1]) {
                 return false;
             }
         }
