3905f3d5d6b74b1418ae79053be315e5f34ecffa
srjm <cyberpiluha@gmail.com>
2025-11-05 09:14:13 +0300

    Слово начинается с ... [#505233 #554123]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,19 +3,13 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startWith(char[] word, char[] prefix) {
         boolean result = true;
-
-        if (prefix.length > word.length) {
-            result = false;
-        } else {
-            for (int i = 0; i < prefix.length; i++) {
-                if (word[i] != prefix[i]) {
-                    result = false;
-                    break;
-                }
+        for (int i = 0; i < prefix.length; i++) {
+            if (word[i] != prefix[i]) {
+                result = false;
+                break;
             }
         }
-
         return result;
-
     }
 }
+
